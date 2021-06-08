c9d2ec57c16296dec4d94d61d83442e3b06957b5
Zubair Omer <omer.zubair01@gmail.com>
2021-06-07 17:14:48 -0700

    header component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './Header'
 
 export default function App() {
   return (
     <div>
+      <Header />
 
+      {/* Nav */}
+
+      {/* Result */}
     </div>
   )
 }
