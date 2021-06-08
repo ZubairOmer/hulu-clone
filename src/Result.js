766dbd191b3be345e5f820f2b895bdfd50d1aca2
Zubair Omer <omer.zubair01@gmail.com>
2021-06-08 01:28:00 -0700

    axios request sent

--- src/Result.js
+++ src/Result.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Result.css'
 import VedioCard from './VedioCard'
+import axios from './axios'
+import requests from './requests'
 
 export default function Result() {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchActionMovies)
+            console.log(request);
+        }
+        fetchData();
+    }, [])
+
     return (
         <div className='result'>
-            <VedioCard />
-            <VedioCard />
-            <VedioCard />
-            <VedioCard />
-            <VedioCard />
-            <VedioCard />
-            <VedioCard />
-            <VedioCard />
-            <VedioCard />
+            {movies.map(movie => (
+                <VedioCard />
+            ))}
         </div>
     )
 }
